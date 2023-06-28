6a35653ab1913acc3fdcb5f17eea4455fb0522f8
Santiago Bedoya <santiagobedoya@live.com>
2023-06-28 17:59:36 -0500

    Terminando la pagina principal

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,8 +1,44 @@
+import { Precio, Categoria, Propiedad } from '../models/index.js'
 
-
-const inicio = (req, res) => {
+const inicio = async (req, res) => {
+  const [categorias, precios, casas, apartamentos] = await Promise.all([
+    Categoria.findAll({ raw: true }),
+    Precio.findAll({ raw: true }),
+    Propiedad.findAll({
+      limit: 3,
+      where: {
+        categoriaId: 1
+      },
+      include: [
+        {
+          model: Precio, as: 'precio'
+        }
+      ],
+      order: [
+        ['createdAt', 'DESC']
+      ]
+    }),
+    Propiedad.findAll({
+      limit: 3,
+      where: {
+        categoriaId: 2
+      },
+      include: [
+        {
+          model: Precio, as: 'precio'
+        }
+      ],
+      order: [
+        ['createdAt', 'DESC']
+      ]
+    })
+  ])
   res.render('inicio', {
-    pagina: 'Inicio'
+    pagina: 'Inicio',
+    categorias,
+    precios,
+    casas,
+    apartamentos
   })
 }
 const categoria = (req, res) => {
--- src/js/mapaInicio.js
+++ src/js/mapaInicio.js
@@ -3,16 +3,33 @@
   const lng = -75.575589
   const mapa = L.map('mapa-inicio').setView([lat, lng], 11.5)
   const markers = new L.FeatureGroup().addTo(mapa)
+  const filtros = {
+    categoria: '',
+    precio: ''
+  }
+  const categoriaSelect = document.querySelector('#categorias')
+  const precioSelect = document.querySelector('#precios')
+  let propiedades = []
+
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(mapa)
 
+  categoriaSelect.addEventListener('change', e => {
+    filtros.categoria = +e.target.value
+    filtrarPropiedades()
+  })
+  precioSelect.addEventListener('change', e => {
+    filtros.precio = +e.target.value
+    filtrarPropiedades()
+  })
+
   const obtenerPropiedades = async () => {
     try {
       const url = '/api/propiedades'
       const respuesta = await fetch(url)
-      const propiedades = await respuesta.json()
+      propiedades = await respuesta.json()
       mostrarPropiedades(propiedades)
     } catch (error) {
       console.log(error)
@@ -20,6 +37,7 @@
   }
 
   const mostrarPropiedades = propiedades => {
+    markers.clearLayers()
     propiedades.forEach(propiedad => {
       const marker = new L.marker([propiedad.lat, propiedad.lng], {
         autoPan: true
@@ -35,6 +53,15 @@
 
       markers.addLayer(marker)
     })
+  }
+
+  const filtrarPropiedades = () => {
+    const resultado = propiedades.filter(propiedad => {
+      return filtros.categoria ? propiedad.categoriaId === filtros.categoria : propiedad
+    }).filter(propiedad => {
+      return filtros.precio ? propiedad.precioId === filtros.precio : propiedad
+    })
+    mostrarPropiedades(resultado)
   }
 
   obtenerPropiedades()
